f31935e2ccbee6a0b444f6a281ce28d14a1b06db
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-17 09:32:31 -0400

    Update game.js
    adding BG_Bed object to scene

--- game.js
+++ game.js
@@ -4,9 +4,9 @@ const config = {
   height: 400,
   parent: 'game-container',
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+    preload,
+    create,
+    update
   },
   physics: {
     default: 'arcade',
@@ -21,15 +21,18 @@ const game = new Phaser.Game(config);
 let player;
 let items;
 let background;
+let bed;
 let collectedIcons = [];
 let targetPosition = null;
-const speed = 4 * 60;
+const speed = 240;
 let collectedCount = 0;
 let lastDirection = 'idle';
 let idleTimer = 0;
+let bedBounds;
 
 function preload() {
   this.load.image('background', 'Background_Grey+.png');
+  this.load.image('bg_bed', 'BG_Bed.png');
   this.load.image('up1', 'up1+.png');
   this.load.image('up2', 'up2+.png');
   this.load.image('down1', 'down1+.png');
@@ -52,12 +55,22 @@ function preload() {
 function create() {
   background = this.add.image(0, 0, 'background').setOrigin(0, 0).setDisplaySize(400, 400);
 
+  // Add bed object
+  bed = this.physics.add.staticImage(32, 200, 'bg_bed').setOrigin(0, 0);
+  bedBounds = bed.getBounds();
+
+  // Debug rectangle for bed
+  const debug = this.add.graphics();
+  debug.lineStyle(2, 0xff0000, 1).strokeRectShape(bedBounds);
+
+  // Player setup
   player = this.physics.add.sprite(200, 200, 'idle1')
     .setCollideWorldBounds(true)
     .setDisplaySize(96, 96);
 
-  player.body.setSize(32, 32).setOffset(1, 1); // Adjusted for larger display
+  player.body.setSize(32, 32).setOffset(1, 1);
 
+  // Animations
   this.anims.create({ key: 'up', frames: [{ key: 'up1' }, { key: 'up2' }], frameRate: 6, repeat: -1 });
   this.anims.create({ key: 'down', frames: [{ key: 'down1' }, { key: 'down2' }], frameRate: 6, repeat: -1 });
   this.anims.create({ key: 'left', frames: [{ key: 'left1' }, { key: 'left2' }], frameRate: 6, repeat: -1 });
@@ -66,17 +79,26 @@ function create() {
 
   player.anims.play('idle');
 
+  // Items group
   items = this.physics.add.group();
   for (let i = 0; i < 3; i++) {
-    const x = Phaser.Math.Between(50, 350);
-    const y = Phaser.Math.Between(50, 350);
+    let x, y, overlap;
+    do {
+      x = Phaser.Math.Between(50, 350);
+      y = Phaser.Math.Between(50, 350);
+      const itemRect = new Phaser.Geom.Rectangle(x - 8, y - 8, 16, 16);
+      overlap = Phaser.Geom.Rectangle.Overlaps(bedBounds, itemRect);
+    } while (overlap);
+
     const item = items.create(x, y, 'item');
     item.setImmovable(true);
     item.body.setCircle(16);
   }
 
   this.physics.add.overlap(player, items, collectItem, null, this);
+  this.physics.add.collider(player, bed);
 
+  // Click to move
   this.input.on('pointerdown', pointer => {
     targetPosition = new Phaser.Math.Vector2(pointer.x, pointer.y);
   });
@@ -95,7 +117,6 @@ function update(time, delta) {
       const dy = targetPosition.y - player.y;
 
       let direction = lastDirection;
-
       if (Math.abs(dx) > Math.abs(dy)) {
         direction = dx > 0 ? 'right' : 'left';
       } else {
@@ -122,7 +143,6 @@ function update(time, delta) {
     }
 
     idleTimer += delta;
-
     if (idleTimer > 1000 && lastDirection !== 'idle') {
       player.anims.play('idle', true);
       lastDirection = 'idle';
@@ -147,12 +167,18 @@ function stopPlayerMovement() {
 function collectItem(player, item) {
   item.destroy();
 
-  const icon = game.scene.scenes[0].add.image(10 + collectedCount * 18, 10, 'icon').setScrollFactor(0).setOrigin(0, 0);
+  const icon = game.scene.scenes[0].add.image(10 + collectedCount * 18, 10, 'icon')
+    .setScrollFactor(0)
+    .setOrigin(0, 0);
+
   collectedIcons.push(icon);
   collectedCount++;
 
   if (collectedCount === 3) {
-    const victoryObject = game.scene.scenes[0].add.image(350, 350, 'item').setScrollFactor(0).setInteractive();
+    const victoryObject = game.scene.scenes[0].add.image(350, 350, 'item')
+      .setScrollFactor(0)
+      .setInteractive();
+
     victoryObject.on('pointerdown', () => {
       alert('🎉 Victory! All items collected!');
       window.mintPrizeNFT?.();
