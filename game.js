d19f873f1d4ff48f7cd3a1cf55ef5993421b98b7
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-17 11:46:22 -0400

    Update game.js

--- game.js
+++ game.js
@@ -65,7 +65,7 @@ function create() {
     .setImmovable(true)
     .setOrigin(0, 0)
     .setDisplaySize(96, 48); // Adjust to match image
-  bedProp.body.setSize(90, 20);
+  bedProp.body.setSize(96, 48);
   bedProp.body.setOffset(4, 28);
 
   this.physics.add.collider(player, bedProp);
@@ -74,8 +74,8 @@ function create() {
   const bed = this.physics.add.sprite(150, 300, 'BG_Bed')
     .setImmovable(true)
     .setOrigin(0, 0)
-    .setDisplaySize(100, 25);
-  bed.body.setSize(100, 25).setOffset(0, 0);
+    .setDisplaySize(120, 60);
+  bed.body.setSize(100, 40).setOffset(0, 0);
   this.physics.add.collider(player, bed);
 
   this.anims.create({ key: 'up', frames: [{ key: 'up1' }, { key: 'up2' }], frameRate: 6, repeat: -1 });
