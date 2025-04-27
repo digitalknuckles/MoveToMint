5e921ec247f941366431a3e98457a5d33e444b3e
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-27 10:07:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -89,7 +89,7 @@ function create() {
   wall.body.setSize(32, 400).setOffset(12, 0);
   this.physics.add.collider(player, wall);
 
-    const wall2 = this.physics.add.sprite(180, -80, 'wall2')
+    const wall2 = this.physics.add.sprite(180, -60, 'wall2')
     .setImmovable(true)
     .setOrigin(0, 0)
     .setDisplaySize(80, 100);
